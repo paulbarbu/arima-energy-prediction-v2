ed39f3e8bf1ad6a381e70443ac6350dce2185512
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2018-10-20 17:09:41 +0300

    added future work, references and rendering to directory

--- report_generator.R
+++ report_generator.R
@@ -3,11 +3,21 @@ library(rmarkdown)
 report <- function(...)
 {
   report.params <- list(...)
+  
   fmt <- paste(paste(rep('%s.', length(report.params)), collapse=''),'html', sep='')
   filename <- do.call(sprintf, c(fmt, report.params))
   
-  print(paste('Generating report file:', filename))
-  render("arima_model.Rmd", params = report.params, output_file = filename, quiet = TRUE)  
+  dirname <- gsub(' ', '_', report.params$series)
+  
+  print(sprintf('Generating report file: %s/%s', dirname, filename))
+  
+  render("arima_model.Rmd",
+         params = report.params,
+         output_file = filename,
+         output_dir = dirname,
+         quiet = TRUE)  
+  
+  print(sprintf('Done: %s/%s', dirname, filename))
 }
 
 report(model = 'Arima(order=c(1, 0, 0))',
@@ -15,3 +25,4 @@ report(model = 'Arima(order=c(1, 0, 0))',
        startday = -10,
        traindays = 7,
        testdays = 3)
+
