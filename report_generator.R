91bc47d6a6afb93d099c168e5f1bb7223420a339
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2018-10-21 12:47:02 +0300

    file path sanitization & params summary at the beginnign of report

--- report_generator.R
+++ report_generator.R
@@ -1,11 +1,12 @@
 library(rmarkdown)
+library(fs)
 
 report <- function(...)
 {
   report.params <- list(...)
   
   fmt <- paste(paste(rep('%s.', length(report.params)), collapse=''),'html', sep='')
-  filename <- do.call(sprintf, c(fmt, report.params))
+  filename <- path_sanitize(do.call(sprintf, c(fmt, report.params)))
   
   dirname <- gsub(' ', '_', report.params$series)
   
@@ -29,10 +30,18 @@ report(model = 'Arima(order=c(1, 0, 0))',
        traindays = 7,
        testdays = 3)
 
-
 report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
        series = '2hrs ph3',
        transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 3)
+
+report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
+       series = '2hrs ph3',
+       transformation = 'log() %>% (function(d) {d[d==-Inf]<-0; d})',
        diffs = 'identity()',
        sdiffs = 'identity()',
        startday = -10,
