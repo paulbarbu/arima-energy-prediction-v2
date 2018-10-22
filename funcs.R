82e29edae86d07670ea8623a79413e3db2f83c37
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2018-10-22 23:55:24 +0300

    generating forecasts frm the full data (this is yet to be thoroughly tested and paralellized) and added reports using regression with Fourier terms and ARIMA errors

--- funcs.R
+++ funcs.R
@@ -9,8 +9,105 @@ get_days <- function(dataset, startday, traindays = 7, testdays=3)
     #print(paste("modified start day = ", startday))
   }
   days <- list()
-  days$train <- window(dataset, start=c(startday, 1), end=c(startday + traindays, 0))
-  days$test <- window(dataset, start=c(startday + traindays, 1), end=c(startday + traindays + testdays, 0))
+  
+  if(is.null(traindays) || traindays == 0)
+  {
+    days$train <- window(dataset, start=c(startday, 1))
+    testdays <- NULL
+  }
+  else
+  {
+    days$train <- window(dataset, start=c(startday, 1), end=c(startday + traindays, 0))
+  }
+  
+  if(is.null(testdays) || testdays == 0)
+  {
+    days$test <- NULL
+  }
+  else
+  {
+    days$test <- window(dataset, start=c(startday + traindays, 1), end=c(startday + traindays + testdays, 0))
+  }
   
   return(days)
+}
+
+report <- function(...)
+{
+  report.params <- list(...)
+  
+  fmt <- paste(paste(rep('%s.', length(report.params)), collapse=''), 'html', sep='')
+  filename <- path_sanitize(do.call(sprintf, c(fmt, report.params)))
+  
+  dirname <- gsub(' ', '_', report.params$series)
+  
+  print(sprintf('Generating report file: %s/%s', dirname, filename))
+  
+  render("arima_model.Rmd",
+         params = report.params,
+         output_file = filename,
+         output_dir = dirname,
+         quiet = TRUE)  
+  
+  print(sprintf('Done: %s/%s', dirname, filename))
+}
+
+report.full <- function(...)
+{
+  report.params <- list(...)
+  
+  fmt <- paste('full_', paste(rep('%s.', length(report.params)), collapse=''), 'html', sep='')
+  filename <- path_sanitize(do.call(sprintf, c(fmt, report.params)))
+  
+  dirname <- gsub(' ', '_', report.params$series)
+  
+  print(sprintf('Generating full data report file: %s/%s', dirname, filename))
+  
+  render("full_forecast_model.Rmd",
+         params = report.params,
+         output_file = filename,
+         output_dir = dirname,
+         quiet = TRUE)  
+  
+  print(sprintf('Done: %s/%s', dirname, filename))
+}
+
+
+#fullforecast(get_days(datasets$`2hrs ph3`$series, 0, 3, 1)$train, 'identity()', 'Arima(order=c(1,1,0))', 1, 1, NULL) -> a
+fullforecast <- function(dataset, transformation, model, traindays, testdays, xreg)
+{
+  startday <- 0
+  endday <- end(dataset)[1] + 1 - (traindays + testdays)
+  
+  fcasts <- list()
+  fcasts$points <- c()
+  
+  #TODO: this can be heavily parallelized
+  for(currentday in startday:endday)
+  {
+    datachunk <- get_days(dataset, currentday, traindays, testdays)
+    
+    eval(parse(text=paste('datachunk$train %>%',
+                          transformation))) -> datachunk$train.transformed
+    
+    eval(parse(text=paste('datachunk$train.transformed %>%', model))) -> fit
+    
+    fourier.terms <- NULL
+    h <- testdays * frequency(datachunk$test)
+    
+    if(!is.null(xreg))
+    {
+      eval(parse(text=paste('datachunk$test %>%', xreg))) -> fourier.terms
+    }
+    
+    fit %>% forecast(h=h, xreg=fourier.terms) -> chunk.fcast
+    fcasts$points <- c(fcasts$points, chunk.fcast$mean)
+  }
+  
+  fcasts$points <- ts(fcasts$points, start=traindays, frequency = frequency(dataset))
+  testpoints <- get_days(dataset, traindays, NULL, NULL)$train
+  
+  fcasts$accuracy <- accuracy(fcasts$points, testpoints)
+  
+  return(fcasts)
 }
--- report_generator.R
+++ report_generator.R
@@ -1,25 +1,10 @@
 library(rmarkdown)
 library(fs)
 
-report <- function(...)
-{
-  report.params <- list(...)
-  
-  fmt <- paste(paste(rep('%s.', length(report.params)), collapse=''),'html', sep='')
-  filename <- path_sanitize(do.call(sprintf, c(fmt, report.params)))
-  
-  dirname <- gsub(' ', '_', report.params$series)
-  
-  print(sprintf('Generating report file: %s/%s', dirname, filename))
-  
-  render("arima_model.Rmd",
-         params = report.params,
-         output_file = filename,
-         output_dir = dirname,
-         quiet = TRUE)  
-  
-  print(sprintf('Done: %s/%s', dirname, filename))
-}
+source('funcs.R')
+
+# 2hrs ph3 Simple Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0)) ----
+# arima(1,0,0)(1,0,0)
 
 report(model = 'Arima(order=c(1, 0, 0))',
        series = '2hrs ph3',
@@ -48,4 +33,216 @@ report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
        traindays = 7,
        testdays = 3)
 
+report(model = 'Arima(order=c(0, 0, 1))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 3)
+
+report(model = 'Arima(order=c(0, 0, 1), seasonal=c(0, 0, 1))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 3)
+
+report(model = 'Arima(order=c(0, 0, 1), seasonal=c(0, 0, 1), xreg=fourier(., K=5))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 3,
+       xreg='fourier(., K=5, h=h)')
+
+report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), xreg=fourier(., K=5))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 3,
+       xreg='fourier(., K=5, h=h)')
+
+report(model = 'Arima(order=c(0, 1, 1), seasonal=c(0, 1, 1))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 3)
+
+report(model = 'Arima(order=c(1, 1, 0), seasonal=c(1, 1, 0))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 3)
+
+report(model = 'auto.arima()',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 3)
+
+# 2hrs ph3 -1 day forecast - Simple Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0)) ----
+# fourier arima(1,0,0)(1,0,0)
+report(model = 'Arima(order=c(1, 0, 0))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1)
+
+report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1)
+
+report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
+       series = '2hrs ph3',
+       transformation = 'log() %>% (function(d) {d[d==-Inf]<-0; d})',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1)
+
+report(model = 'Arima(order=c(0, 0, 1))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1)
+
+report(model = 'Arima(order=c(0, 0, 1), seasonal=c(0, 0, 1))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1)
+
+report(model = 'Arima(order=c(0, 0, 1), seasonal=c(0, 0, 1), xreg=fourier(., K=5))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1,
+       xreg='fourier(., K=5, h=h)')
+
+report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), xreg=fourier(., K=5))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1,
+       xreg='fourier(., K=5, h=h)')
+
+report(model = 'Arima(order=c(0, 1, 1), seasonal=c(0, 1, 1))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1)
+
+report(model = 'Arima(order=c(1, 1, 0), seasonal=c(1, 1, 0))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1)
+
+report(model = 'auto.arima()',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = -10,
+       traindays = 7,
+       testdays = 1)
+
+# 2hrs ph3 from the front ----
+
+report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = 0,
+       traindays = 7,
+       testdays = 1)
+
+report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), xreg=fourier(., K=5))',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = 0,
+       traindays = 7,
+       testdays = 1,
+       xreg='fourier(., K=5, h=h)')
+
+report(model = 'auto.arima()',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = 0,
+       traindays = 7,
+       testdays = 1)
+
+
 #TODO: auto.arima for all data
+#TODO: set K to an interval in order to find the best one, how to choose it?
+
+# 2hrs ph3 full ----
+report.full(model = 'auto.arima()',
+       series = '2hrs ph3',
+       transformation = 'identity()',
+       traindays = 7,
+       testdays = 1)
+
+#https://stackoverflow.com/questions/7233288/non-stationary-seasonal-ar-part-from-css-error-in-r
+report.full(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="ML")',
+            series = '2hrs ph3',
+            transformation = 'identity()',
+            traindays = 7,
+            testdays = 1)
+
+report.full(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="ML", xreg=fourier(., K=5))',
+            series = '2hrs ph3',
+            transformation = 'identity()',
+            traindays = 7,
+            testdays = 1,
+            xreg='fourier(., K=5, h=h)')
