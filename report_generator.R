bad6913d7f3875b399c5879c715db44b359e3ad1
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2018-10-25 17:26:11 +0300

    added some explanations on how I arrived at those particular models

--- report_generator.R
+++ report_generator.R
@@ -6,6 +6,7 @@ source('funcs.R')
 # 2hrs ph3 Simple Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0)) ----
 # arima(1,0,0)(1,0,0)
 
+# One positive lag in ACF & PACF - AR signature
 report(model = 'Arima(order=c(1, 0, 0))',
        series = '2hrs ph3',
        transformation = 'identity()',
@@ -15,6 +16,7 @@ report(model = 'Arima(order=c(1, 0, 0))',
        traindays = 7,
        testdays = 3)
 
+# Previous model had residual significant at lag = 12 (24hrs seasonality)
 report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
        series = '2hrs ph3',
        transformation = 'identity()',
@@ -24,6 +26,7 @@ report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
        traindays = 7,
        testdays = 3)
 
+# Try with a transformation, avoid -Inf values
 report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
        series = '2hrs ph3',
        transformation = 'log() %>% (function(d) {d[d==-Inf]<-0; d})',
@@ -33,6 +36,8 @@ report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
        traindays = 7,
        testdays = 3)
 
+# Try also with a MA term since neither the ACF nor the PACF
+# decays slowly or sinusoidally in order to be sure AR or MA is definitely usable
 report(model = 'Arima(order=c(0, 0, 1))',
        series = '2hrs ph3',
        transformation = 'identity()',
@@ -42,6 +47,8 @@ report(model = 'Arima(order=c(0, 0, 1))',
        traindays = 7,
        testdays = 3)
 
+# Same as above, but with seasonal terms, again there were significant residuals 
+# at lag 12
 report(model = 'Arima(order=c(0, 0, 1), seasonal=c(0, 0, 1))',
        series = '2hrs ph3',
        transformation = 'identity()',
@@ -51,6 +58,8 @@ report(model = 'Arima(order=c(0, 0, 1), seasonal=c(0, 0, 1))',
        traindays = 7,
        testdays = 3)
 
+#Fourier terms for the model AR(1) and SAR(1)
+#K was randomly chosen by me
 report(model = 'Arima(order=c(0, 0, 1), seasonal=c(0, 0, 1), xreg=fourier(., K=5))',
        series = '2hrs ph3',
        transformation = 'identity()',
@@ -61,6 +70,8 @@ report(model = 'Arima(order=c(0, 0, 1), seasonal=c(0, 0, 1), xreg=fourier(., K=5
        testdays = 3,
        xreg='fourier(., K=5, h=h)')
 
+#Fourier terms for the model MA(1) and SMA(1)
+#K was randomly chosen by me
 report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), xreg=fourier(., K=5))',
        series = '2hrs ph3',
        transformation = 'identity()',
@@ -71,6 +82,7 @@ report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), xreg=fourier(., K=5
        testdays = 3,
        xreg='fourier(., K=5, h=h)')
 
+#Try with differences, although no indication of trend or seasonal-trend
 report(model = 'Arima(order=c(0, 1, 1), seasonal=c(0, 1, 1))',
        series = '2hrs ph3',
        transformation = 'identity()',
@@ -80,6 +92,7 @@ report(model = 'Arima(order=c(0, 1, 1), seasonal=c(0, 1, 1))',
        traindays = 7,
        testdays = 3)
 
+#Variation on the above
 report(model = 'Arima(order=c(1, 1, 0), seasonal=c(1, 1, 0))',
        series = '2hrs ph3',
        transformation = 'identity()',
@@ -222,7 +235,7 @@ report(model = 'auto.arima()',
        traindays = 7,
        testdays = 1)
 
-# 2hrs ph3 full ----
+# 2hrs ph3 full - apply the best models so far on the full data----
 report.full(model = 'auto.arima()',
        series = '2hrs ph3',
        transformation = 'identity()',
@@ -230,12 +243,16 @@ report.full(model = 'auto.arima()',
        testdays = 1)
 
 #https://stackoverflow.com/questions/7233288/non-stationary-seasonal-ar-part-from-css-error-in-r
+
+# This model predicts the new value as a multiple of the previous (seasonal) value plus a constant
+# This model assumes mean reverting behaviour (no trend - no diffs) which for house-hold is true (energy consumption is the same, on average)
 report.full(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="ML")',
             series = '2hrs ph3',
             transformation = 'identity()',
             traindays = 7,
             testdays = 1)
 
+# Fit the seasonality using fourier terms nad the errors using the logic above
 report.full(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="ML", xreg=fourier(., K=5))',
             series = '2hrs ph3',
             transformation = 'identity()',
