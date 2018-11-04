72d93c2ff33bd4a7c0c734e0835f97bd471da333
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2018-11-04 22:40:21 +0200

    added serial fullforecast function

--- funcs.R
+++ funcs.R
@@ -1,6 +1,8 @@
+library(forecast)
 library(foreach)
 library(doParallel)
-library(forecast)
+
+source('datasets.R')
 
 # series= datasets[['1hrs ph3']]$series; x = get_days(series, 0, 1, 1); View(series); View(x$train); View(x$test)
 # series= datasets[['1hrs ph3']]$series; x = get_days(series, 1, 1, 1); View(series); View(x$train); View(x$test)
@@ -87,7 +89,7 @@ fullforecast <- function(dataset, transformation, model, traindays, testdays, xr
   
   numcores <- max(1, detectCores() - 1)
   
-  cl<-makeCluster(numcores)
+  cl <- makeCluster(numcores)
   registerDoParallel(cl)
   
   print(paste('Running on', numcores, 'cores')) 
@@ -98,6 +100,7 @@ fullforecast <- function(dataset, transformation, model, traindays, testdays, xr
                            .combine = c, 
                            .verbose = TRUE) %dopar%
   {
+    print(paste("Current day =", currentday))
     datachunk <- get_days(dataset, currentday, traindays, testdays)
     
     eval(parse(text=paste('datachunk$train %>%',
@@ -139,6 +142,65 @@ fullforecast <- function(dataset, transformation, model, traindays, testdays, xr
   }
   
   stopCluster(cl)
+  
+  fcasts$points <- ts(fcasts$points, start=traindays, frequency = frequency(dataset))
+  testpoints <- get_days(dataset, traindays, NULL, NULL)$train
+  
+  fcasts$accuracy <- accuracy(fcasts$points, testpoints)
+  
+  return(fcasts)
+}
+
+fullforecast.serial <- function(dataset, transformation, model, traindays, testdays, xreg)
+{
+  startday <- 0
+  endday <- end(dataset)[1] + 1 - (traindays + testdays)
+  
+  fcasts <- list()
+  fcasts$points <- c()
+  
+  for(currentday in startday:endday)
+  {
+    print(paste("Current day =", currentday))
+    datachunk <- get_days(dataset, currentday, traindays, testdays)
+    
+    eval(parse(text=paste('datachunk$train %>%',
+                         transformation))) -> datachunk$train.transformed
+    
+    h <- testdays * frequency(datachunk$test)
+    
+    # for naive, snaive and meanf models
+    # the model already returns the forecasts, no separate forecast step
+    # is needed
+    if(any(startsWith(model, c("naive()", "snaive()", "meanf()"))))
+    {
+     model <- sub("()", paste("(h=", h, ")", sep=""), model, fixed=TRUE)
+    } 
+    
+    eval(parse(text=paste('datachunk$train.transformed %>%', model))) -> fit
+    
+    fcast <- NULL
+    
+    # for naive, snaive and meanf models
+    # the model already returns the forecasts, no separate forecast step
+    # is needed
+    if(any(startsWith(model, c("naive(", "snaive(", "meanf("))))
+    {
+     chunk.fcast <- fit
+    } else
+    {
+     fourier.terms <- NULL
+     
+     if(!is.null(xreg))
+     {
+       eval(parse(text=paste('datachunk$test %>%', xreg))) -> fourier.terms
+     }
+     
+     fit %>% forecast(h=h, xreg=fourier.terms) -> chunk.fcast
+    }
+    
+    fcasts$points <- c(fcasts$points, chunk.fcast$mean)
+  }
   
   fcasts$points <- ts(fcasts$points, start=traindays, frequency = frequency(dataset))
   testpoints <- get_days(dataset, traindays, NULL, NULL)$train
