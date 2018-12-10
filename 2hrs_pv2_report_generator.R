ff1ae4ba2e99d7e58db37137698aa50b1c3f3710
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2018-12-10 18:25:53 +0200

    added the report generator for pv2 and its best runs

--- 2hrs_pv2_report_generator.R
+++ 2hrs_pv2_report_generator.R
@@ -3,109 +3,105 @@ source('funcs.R')
 # benchmark models  ----
 
 report.full(model = 'snaive()',
-            series = '2hrs ph1',
+            series = '2hrs pv2',
             transformation = 'identity()',
-            traindays = 7,
+            traindays = 5,
             testdays = 1)
 
 report.full(model = 'meanf()',
-            series = '2hrs ph1',
+            series = '2hrs pv2',
             transformation = 'identity()',
-            traindays = 7,
+            traindays = 5,
             testdays = 1)
 
 report.full(model = 'naive()',
-            series = '2hrs ph1',
+            series = '2hrs pv2',
             transformation = 'identity()',
-            traindays = 7,
+            traindays = 5,
             testdays = 1)
 
 # try to find the best ARIMA model  ----
-
-# sinusoidal ACF and 1 lag in PACF from the beginning of the dataset
 report(model = 'Arima(order=c(1, 0, 0))',
-       series = '2hrs ph1',
+       series = '2hrs pv2',
        transformation = 'identity()',
        diffs = 'identity()',
        sdiffs = 'identity()',
        startday = 0,
-       traindays = 7,
+       traindays = 5,
        testdays = 3)
 
-# sinusoidal ACF and 1 lag in PACF
-report(model = 'Arima(order=c(1, 0, 0))',
-       series = '2hrs ph1',
+# sinusoidal ACF and 2 lag in PACF from the beginning of the dataset, but also lambda= 0.2654112
+report(model = 'Arima(order=c(2, 0, 0), lambda=0.2654112)',
+       series = '2hrs pv2',
        transformation = 'identity()',
        diffs = 'identity()',
        sdiffs = 'identity()',
-       startday = -10,
-       traindays = 7,
+       startday = 0,
+       traindays = 5,
        testdays = 3)
 
-# significant values in lags of order 12
+# sinusoidal ACF and 2 lag in PACF from the beginning of the dataset
+report(model = 'Arima(order=c(2, 0, 0))',
+       series = '2hrs pv2',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = 0,
+       traindays = 5,
+       testdays = 3)
+
+# significant term in PACF, so add 1 SAR term
+report(model = 'Arima(order=c(2, 0, 0), seasonal=c(1, 0, 0))',
+       series = '2hrs pv2',
+       transformation = 'identity()',
+       diffs = 'identity()',
+       sdiffs = 'identity()',
+       startday = 0,
+       traindays = 5,
+       testdays = 3)
+
+# try to reduce the AR term, indeed, better results
 report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0))',
-       series = '2hrs ph1',
+       series = '2hrs pv2',
        transformation = 'identity()',
        diffs = 'identity()',
        sdiffs = 'identity()',
-       startday = -10,
-       traindays = 7,
+       startday = 0,
+       traindays = 5,
        testdays = 3)
 
-# two significant lags around the seasonal period
-report(model = 'Arima(order=c(1, 0, 0), seasonal=c(2, 0, 0), method="CSS")',
-       series = '2hrs ph1',
+# add fourier terms
+report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), xreg=fourier(., K=2))',
+       series = '2hrs pv2',
        transformation = 'identity()',
        diffs = 'identity()',
        sdiffs = 'identity()',
-       startday = -10,
-       traindays = 7,
-       testdays = 3)
-
-# seems fine, but the forecast is overall a bit too high
-report(model = 'Arima(order=c(1, 0, 0), seasonal=c(2, 0, 0), include.mean=FALSE, method="CSS")',
-       series = '2hrs ph1',
-       transformation = 'identity()',
-       diffs = 'identity()',
-       sdiffs = 'identity()',
-       startday = -10,
-       traindays = 7,
-       testdays = 3)
-
-
-# regression with ARMA errors on fourier terms, (tries with 2 seasonal AR terms, but results were worse)
-report(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), xreg=fourier(., K=2), method="CSS")',
-       series = '2hrs ph1',
-       transformation = 'identity()',
-       diffs = 'identity()',
-       sdiffs = 'identity()',
-       startday = -10,
-       traindays = 7,
+       startday = 0,
+       traindays = 5,
        testdays = 3,
        xreg='fourier(., K=2, h=h)')
 
-
 #  Find the best train:test days ratio for ARIMA(1,0,0)(1,0,0) ----
 
-best.fcast.2hrsPh1 <- NULL
+best.fcast.2hrsPv2 <- NULL
 best.traindays <- 0
 best.testdays <- 0
 
 for(traindays in 3:7)
 {
-  for(testdays in 1:3)
+  for(testdays in 2:3)
   {
     print(paste("Trying", traindays, "train days and", testdays, "test days"))
     current <- fullforecast(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="CSS")',
-                            dataset = datasets[['2hrs ph1']]$series,
+                            dataset = datasets[['2hrs pv2']]$series,
                             transformation = 'identity()',
                             traindays = traindays,
                             testdays = testdays,
                             xreg=NULL)
     
-    if(is.null(best.fcast.2hrsPh1) || current$accuracy[[2]] < best.fcast.2hrsPh1$accuracy[[2]])
+    if(is.null(best.fcast.2hrsPv2) || current$accuracy[[2]] < best.fcast.2hrsPv2$accuracy[[2]])
     {
-      best.fcast.2hrsPh1 <- current
+      best.fcast.2hrsPv2 <- current
       best.traindays <- traindays
       best.testdays <- testdays
     }
@@ -113,91 +109,115 @@ for(traindays in 3:7)
 }
 
 report.full(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="ML")',
-            series = '2hrs ph1',
+            series = '2hrs pv2',
             transformation = 'identity()',
-            traindays = best.traindays, # 7
+            traindays = best.traindays, # 5
             testdays = best.testdays) # 2
 
 # Skip over the step where I hardcode a fourier value ----
 # Find best K for the above model ARIMA(1,0,0)(1,0,0) ----
 
-best.fcast.k.2hrsPh1 <- NULL
+best.fcast.k.2hrsPv2 <- NULL
 best.k <- 0
 #K must be not be greater than period/2
-for(k in 1:(frequency(datasets[['2hrs ph1']]$series)/2))
+for(k in 1:(frequency(datasets[['2hrs pv2']]$series)/2))
 {
   print(paste("Trying k =", k))
   m <- paste0('Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="CSS", xreg=fourier(., K=', k, '))')
   xreg <- paste0('fourier(., h=h, K=', k, ')')
   current <- fullforecast(model = m,
-                          dataset = datasets[['2hrs ph1']]$series,
+                          dataset = datasets[['2hrs pv2']]$series,
                           transformation = 'identity()',
-                          traindays = best.traindays, # 7
+                          traindays = best.traindays, # 5
                           testdays = best.testdays, # 2
                           xreg=xreg)
   
-  if(is.null(best.fcast.k.2hrsPh1) || current$accuracy[[2]] < best.fcast.k.2hrsPh1$accuracy[[2]])
+  if(is.null(best.fcast.k.2hrsPv2) || current$accuracy[[2]] < best.fcast.k.2hrsPv2$accuracy[[2]])
   {
-    best.fcast.k.2hrsPh1 <- current
+    best.fcast.k.2hrsPv2 <- current
     best.k <- k
   }
 }
 
 report.full(model = paste('Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="CSS", xreg=fourier(., K=', best.k, '))', sep=''),
-            series = '2hrs ph1',
+            series = '2hrs pv2',
             transformation = 'identity()',
-            traindays = best.traindays, # 7
+            traindays = best.traindays, # 5
             testdays = best.testdays, # 2
-            xreg = paste('fourier(., h=h, K=', best.k, ')')) #2
+            xreg = paste('fourier(., h=h, K=', best.k, ')')) # 2
 
-# Best model: 7:2, ARIMA(1, 0, 0)(1, 0, 0), K=2, RMSE=469.4395 	MAE=223.396  ----
-# 6th-9th obs dummies rmse=464, mae=222
-# without seasonal part, only fourier: rmse=467, mae=220
-# dummies without seasonal part, only fourier: rmse=462, mae=218
+# Best model: 5:2, ARIMA(1, 0, 0)(1, 0, 0), K=2, RMSE=479.5438 MAE=238.0388  ----
+# 4th-4th obs dummies rmse=478, mae=239
+# without seasonal part, only fourier: rmse=457, mae=234
+# without seasonal part, 2AR, only fourier: rmse=458, mae=234
+# dummies without seasonal part, only fourier: rmse=456, mae=235
+# dummies without seasonal part, 2AR, only fourier: rmse=456, mae=235
 report.full(model = 'Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="CSS", xreg=fourier(., K=2))',
-            series = '2hrs ph1',
+            series = '2hrs pv2',
             transformation = 'identity()',
-            traindays = 7,
+            traindays = 5,
             testdays = 2,
             xreg = 'fourier(., h=h, K=2)')
 
 dummies.fcast <- quote(
   {cbind(
-    dummies=getNthObsDummies(6, 3, h, frequency(.)),
+    dummies=getNthObsDummies(4, 4, h, frequency(.)),
     fourier(., h=h, K=2)
   )}
 )
 
 dummies.fit <- quote(
   {cbind(
-    dummies=getNthObsDummies(6, 3, length(.), frequency(.)),
+    dummies=getNthObsDummies(4, 4, length(.), frequency(.)),
     fourier(., K=2)
   )}
 )
 
 report.full(model = paste0('Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="CSS", xreg=', paste0(deparse(dummies.fit), collapse='') ,')'),
-  series = '2hrs ph1',
-  transformation = 'identity()',
-  traindays = 7,
-  testdays = 2,
-  xreg = paste0(deparse(dummies.fcast), collapse=''))
-
-#rmse=462, mae=218
-report.full(model = paste0('Arima(order=c(1, 0, 0), method="CSS", xreg=', paste0(deparse(dummies.fit), collapse='') ,')'),
-  series = '2hrs ph1',
-  transformation = 'identity()',
-  traindays = 7,
-  testdays = 2,
-  xreg = paste0(deparse(dummies.fcast), collapse=''))
-
-# rmse=467, mae=220
-report.full(model = 'Arima(order=c(1, 0, 0), method="CSS", xreg=fourier(., K=2))',
-            series = '2hrs ph1',
+            series = '2hrs pv2',
             transformation = 'identity()',
-            traindays = 7,
+            traindays = 5,
+            testdays = 2,
+            xreg = paste0(deparse(dummies.fcast), collapse=''))
+
+# NO SAR, with dummies 
+# rmse=456, mae=235
+report.full(model = paste0('Arima(order=c(1, 0, 0), method="CSS", xreg=', paste0(deparse(dummies.fit), collapse='') ,')'),
+            series = '2hrs pv2',
+            transformation = 'identity()',
+            traindays = 5,
+            testdays = 2,
+            xreg = paste0(deparse(dummies.fcast), collapse=''))
+
+# no SAR with dummies
+# 2 AR terms
+# rmse=456, mae=235
+report.full(model = paste0('Arima(order=c(2, 0, 0), method="CSS", xreg=', paste0(deparse(dummies.fit), collapse='') ,')'),
+            series = '2hrs pv2',
+            transformation = 'identity()',
+            traindays = 5,
+            testdays = 2,
+            xreg = paste0(deparse(dummies.fcast), collapse=''))
+
+
+# no SAR, no dummies
+# rmse=457, mae=234
+report.full(model = 'Arima(order=c(1, 0, 0), method="CSS", xreg=fourier(., K=2))',
+            series = '2hrs pv2',
+            transformation = 'identity()',
+            traindays = 5,
             testdays = 2,
             xreg = 'fourier(., h=h, K=2)')
-# dummies on 6th day - not applicable, patterns are too crazy ----
+
+# no SAR, no dummies, 2 AR terms
+# rmse=458, mae=234
+report.full(model = 'Arima(order=c(2, 0, 0), method="CSS", xreg=fourier(., K=2))',
+            series = '2hrs pv2',
+            transformation = 'identity()',
+            traindays = 5,
+            testdays = 2,
+            xreg = 'fourier(., h=h, K=2)')
+
 
 # dummies on every weekday ----
 
@@ -215,17 +235,17 @@ dailyD.fit <- quote(
   )}
 )
 
-# 7:2 rmse=486, mae=242
+# 5:2 rmse=?, mae=?
 report.full(model = paste0('Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="CSS", xreg=', paste0(deparse(dailyD.fit), collapse='') ,')'),
-            series = '2hrs ph1',
+            series = '2hrs pv2',
             transformation = 'identity()',
-            traindays = 7,
+            traindays = 5,
             testdays = 2,
             xreg = paste0(deparse(dailyD.fcast), collapse=''))
 
-# dummies on the 4-8th+1-5th obs (the "outliers") ----
+# dummies on the ?-?th+?-?th obs (the "outliers") ----
 
-best.fcast.dummy.2hrsPh1 <- NULL
+best.fcast.dummy.2hrsPv2 <- NULL
 best.startDummy <- 0
 best.lenDummy <- 0
 
@@ -252,15 +272,15 @@ for(startDummy in 4:8)
     )
     
     current <- fullforecast(model = paste0('Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="CSS", xreg=', paste0(deparse(obsDummies.fit), collapse='') ,')'),
-                            dataset = datasets[['2hrs ph1']]$series,
+                            dataset = datasets[['2hrs pv2']]$series,
                             transformation = 'identity()',
-                            traindays = 7,
+                            traindays = 5,
                             testdays = 2,
                             xreg = paste0(deparse(obsDummies.fcast), collapse=''))
     
-    if(is.null(best.fcast.dummy.2hrsPh1) || current$accuracy[[2]] < best.fcast.dummy.2hrsPh1$accuracy[[2]])
+    if(is.null(best.fcast.dummy.2hrsPv2) || current$accuracy[[2]] < best.fcast.dummy.2hrsPv2$accuracy[[2]])
     {
-      best.fcast.dummy.2hrsPh1 <- current
+      best.fcast.dummy.2hrsPv2 <- current
       best.startDummy <- startDummy
       best.lenDummy <- lenDummy
     }
@@ -284,11 +304,10 @@ bestObsDummies.fit <- substitute(
   list(best.startDummy = best.startDummy, best.lenDummy = best.lenDummy)
 )
 
-# 7:2, dummies: 6:3, rmse=464, mae=222
+# 5:2, dummies: 4:4, rmse=478, mae=239
 report.full(model = paste0('Arima(order=c(1, 0, 0), seasonal=c(1, 0, 0), method="CSS", xreg=', paste0(deparse(bestObsDummies.fit), collapse='') ,')'),
-            series = '2hrs ph1',
+            series = '2hrs pv2',
             transformation = 'identity()',
-            traindays = 7,
+            traindays = 5,
             testdays = 2,
             xreg = paste0(deparse(bestObsDummies.fcast), collapse=''))
-
